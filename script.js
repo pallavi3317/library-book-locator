572eb0b9042cbd919625baf959c044707fc77ec2
Pallavi <pallavigwda33@gmail.com>
2026-07-01 19:45:02 +0530

    Added JavaScript for homepage

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const searchButton = document.getElementById("searchBtn");
+
+    if (searchButton) {
+
+        searchButton.addEventListener("click", function () {
+
+            alert("Welcome to Library Book Locator! Search page will open soon.");
+
+        });
+
+    }
+
+});
